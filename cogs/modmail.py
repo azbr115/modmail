eecff5538cddd73635558fced714888e04e06524
azbr115 <81177938+azbr115@users.noreply.github.com>
2021-07-26 00:15:53 +0100

    Update modmail.py

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -1006,7 +1006,7 @@ class Modmail(commands.Cog):
         if exists:
             embed = discord.Embed(
                 color=self.bot.error_color,
-                description="A thread for this user already " f"exists in {exists.channel.mention}.",
+                description="Uma thread para este utilizador já" f"existe em {exists.channel.mention}.",
             )
             await ctx.channel.send(embed=embed, delete_after=3)
 
